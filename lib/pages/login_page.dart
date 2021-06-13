0ae7a3d050c9e9357eeaaa25f8ef3a1fc296c34a
Karan Vaswani <53395550+karan-17@users.noreply.github.com>
2021-06-14 01:56:33 +0530

    day6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,93 +1,94 @@
 import 'package:codeytapp/utils/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+
   String name = "";
   bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Welcome $name",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        hintText: "Enter Username",
-                        labelText: "Username",
-                      ),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
                     ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                      ),
+                    onChanged: (value){
+                      name=value;
+                      setState(() {
+
+                      });
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
                     ),
-                    SizedBox(
-                      height: 40.0,
-                    ),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
                     InkWell(
-                      onTap: () {
+                      onTap: () async {
                         setState(() {
-                          changeButton = true;
+                          changeButton=true;
                         });
-                        // Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        await Future.delayed(Duration(seconds: 1));
+                       // Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: AnimatedContainer(
-                        duration: Duration(seconds: 1),
-                        width: changeButton ? 50 : 150,
-                        height: 50,
-                        alignment: Alignment.center,
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                        decoration: BoxDecoration(
-                          shape: changeButton
-                              ? BoxShape.circle
-                              : BoxShape.rectangle,
-                          color: Colors.deepPurple,
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton?50:150,
+                      height:50,
+                      alignment: Alignment.center,
+                      child: changeButton?Icon(Icons.done, color: Colors.white,): Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
                         ),
                       ),
-                    )
-                    /*ElevatedButton(
+                      decoration: BoxDecoration(
+                        shape: changeButton?BoxShape.circle:BoxShape.rectangle,
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(changeButton?50:8,),
+                      ),
+                    ),
+                  )
+                  /*ElevatedButton(
                       onPressed: (){
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
@@ -96,11 +97,12 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                       child: Text("Login")
                   ),*/
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
   }
 }
